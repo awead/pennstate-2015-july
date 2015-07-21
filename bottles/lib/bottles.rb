685b8969b6311e46863af1a738660072ac371424
Adam Wead <amsterdamos@gmail.com>
2015-07-21 15:04:54 -0400

    shamless green

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -2,26 +2,11 @@ class Bottles
 
   def verse(num)
     case num
-    when 99
-      "99 bottles of beer on the wall, " +
-      "99 bottles of beer.\n" +
-      "Take one down and pass it around, " +
-      "98 bottles of beer on the wall.\n"
-    when 98
-      "98 bottles of beer on the wall, " +
-      "98 bottles of beer.\n" +
-      "Take one down and pass it around, " +
-      "97 bottles of beer on the wall.\n"
-    when 3
-      "3 bottles of beer on the wall, " +
-      "3 bottles of beer.\n" +
-      "Take one down and pass it around, " +
-      "2 bottles of beer on the wall.\n"
     when 2
       "2 bottles of beer on the wall, " +
       "2 bottles of beer.\n" +
       "Take one down and pass it around, " +
-      "1 bottle of beer on the wall.\n"
+      "1 bottle of beer on the wall.\n"    
     when 1
       "1 bottle of beer on the wall, " +
       "1 bottle of beer.\n" +
@@ -32,6 +17,11 @@ class Bottles
       "no more bottles of beer.\n" +
       "Go to the store and buy some more, " +
       "99 bottles of beer on the wall.\n"
+    else
+      "#{num} bottles of beer on the wall, " +
+      "#{num} bottles of beer.\n" +
+      "Take one down and pass it around, " +
+      "#{num-1} bottles of beer on the wall.\n"    
     end
   end
 
@@ -39,4 +29,7 @@ class Bottles
     high.downto(low).map { |n| verse(n) }.join("\n")
   end
 
+  def song
+    verses(99, 0)
+  end
 end
