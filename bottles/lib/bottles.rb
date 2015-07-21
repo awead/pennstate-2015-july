1035d49789487b00d3970a755779e5ec3bbeb7de
Adam Wead <amsterdamos@gmail.com>
2015-07-21 16:29:42 -0400

    save

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -2,26 +2,34 @@ class Bottles
 
   def verse(num)
     case num
-    when 2
-      "2 bottles of beer on the wall, " +
-      "2 bottles of beer.\n" +
-      "Take one down and pass it around, " +
-      "1 bottle of beer on the wall.\n"    
-    when 1
-      "1 bottle of beer on the wall, " +
-      "1 bottle of beer.\n" +
-      "Take it down and pass it around, " +
-      "no more bottles of beer on the wall.\n"
     when 0
-      "No more bottles of beer on the wall, " +
-      "no more bottles of beer.\n" +
-      "Go to the store and buy some more, " +
-      "99 bottles of beer on the wall.\n"
+      "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
     else
-      "#{num} bottles of beer on the wall, " +
-      "#{num} bottles of beer.\n" +
-      "Take one down and pass it around, " +
-      "#{num-1} bottles of beer on the wall.\n"    
+      "#{num} #{container(num)} of beer on the wall, #{num} #{container(num)} of beer.\nTake #{pronoun(num)} down and pass it around, #{quantity(num - 1)} #{container(num - 1)} of beer on the wall.\n"
+    end
+  end
+
+  def quantity(num)
+    if num == 0
+      "no more"
+    else
+      num
+    end
+  end
+
+  def pronoun(num)
+    if num == 1
+      "it"
+    else
+      "one"
+    end
+  end
+
+  def container(num)
+    if num == 1
+      "bottle"
+    else
+      "bottles"
     end
   end
 
